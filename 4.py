17366d586ed6f1fbba7b6d5828bcd867198b62d1
Artem Demkin <114858955+ArtDemkin@users.noreply.github.com>
2023-01-01 03:13:54 +0300

    Update 4.py

--- 4.py
+++ 4.py
@@ -1,6 +1,10 @@
+import pandas as pd
+import streamlit as st
 from transformers import pipeline
 classifier = pipeline("sentiment-analysis", "blanchefort/rubert-base-cased-sentiment")
 df = open('chats.pkl', 'r+', encoding="UTF-8")
+st.title('Классификация изображений')
 for line in df:
     category = classifier([line])
     print(line, category)
+    st.write(line, category)
