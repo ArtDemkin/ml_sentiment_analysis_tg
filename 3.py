592c0dc1271c8618dccc1613da11c66248e03d12
Artem Demkin <artemonuys@gmail.com>
2023-01-08 19:35:31 +0300

    commit

--- 3.py
+++
@@ -1,3 +0,0 @@
-import pandas as pd
-df = pd.read_csv('chats.csv', delimiter = ',', encoding="UTF-8")#основной набор данных
-print(df.astype)
--- 4.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-from transformers import pipeline
-classifier = pipeline("sentiment-analysis", "blanchefort/rubert-base-cased-sentiment")
-df = open('chats.csv', 'r+', encoding="UTF-8")
-st.title('Классификация изображений')
-for line in df:
-    category = classifier([line])
-    print(line, category)
-    st.write(line, category)
