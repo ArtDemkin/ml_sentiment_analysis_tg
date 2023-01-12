8663b9f6913961cd4ec3828416513da5eb5acb0c
Artem Demkin <artemonuys@gmail.com>
2023-01-12 14:58:50 +0300

    commit

--- st_app.py
+++ st_app.py
@@ -1,7 +1,7 @@
 from transformers import pipeline
 import streamlit as st
 classifier = pipeline("sentiment-analysis", "blanchefort/rubert-base-cased-sentiment")
-st.title('Классификация изображений')
+st.title('Анализ тональности текста сообщений из группы телеграмм')
 with open('chats.csv', 'r+', encoding="UTF-8") as df:
     for line in df:
         category = classifier([line])
