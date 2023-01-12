ac8c57d3a2413a8d525b8683775bb33801986238
Artem Demkin <artemonuys@gmail.com>
2023-01-12 14:36:30 +0300

    commit

--- mein.py
+++ mein.py
@@ -39,28 +39,7 @@ for g in groups:
 g_index = 0
 target_group=groups[int(g_index)]
 print("Узнаём пользователей...")
-all_participants = []
-all_participants = client.get_participants(target_group)
-print("Сохраняем данные в файл...")
-with open("members.csv", "w", encoding="UTF-8", newline='\n') as f:
-   writer = csv.writer(f,delimiter=",",lineterminator="\n")
-   writer.writerow(["username", "name","group"])
-   for user in all_participants:
-       if user.username:
-           username= user.username
-       else:
-           username= ""
-       if user.first_name:
-           first_name= user.first_name
-       else:
-           first_name= ""
-       if user.last_name:
-           last_name= user.last_name
-       else:
-           last_name= ""
-       name= (first_name + ' ' + last_name).strip()
-       writer.writerow([username,name,target_group.title])     
-print("Парсинг участников группы успешно выполнен.")
+
  
 offset_id = 0
 limit = 100
@@ -89,7 +68,7 @@ while True:
        break
   
 print("Сохраняем данные в файл...")
-with open("chats.csv", "w", encoding="UTF-8", newline='\n'):
+with open("chats.csv", "w", encoding="UTF-8", newline='\n') as f:
    writer = csv.writer(f, delimiter=",", lineterminator="\n")
    for message in all_messages:
        writer.writerow([message])  
