4b7c7f9d54937fc1d292d565f8a38ccada8ef5c7
ArtDemkin <artemonuys@gmail.com>
2023-04-16 15:03:19 +0300

    Refactoring 1. Wrap the 'with for' block in the strings_analize() function for better understanding.

--- st_app.py
+++ st_app.py
@@ -3,8 +3,14 @@ import streamlit as st
 
 classifier = pipeline("sentiment-analysis", "blanchefort/rubert-base-cased-sentiment")
 st.title('Анализ тональности текста сообщений из группы телеграмм')
-with open('chats.csv', 'r+', encoding="UTF-8") as df:
-    for line in df:
-        category = classifier([line])
-        print(line, category)
-        st.write(line, category)
+
+
+def strings_analize():
+    with open('chats.csv', 'r+', encoding="UTF-8") as df:
+        for line in df:
+            category = classifier([line])
+            print(line, category)
+            st.write(line, category)
+
+
+strings_analize()
