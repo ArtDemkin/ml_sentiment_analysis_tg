592a7ce32fca002a99d8905f957f9baadcc8047e
ArtDemkin <artemonuys@gmail.com>
2023-04-16 15:08:25 +0300

    Refactoring 2. Move the path to the file with strings into a separate variable path_to_strings from the strings_analize() function for better understanding

--- st_app.py
+++ st_app.py
@@ -3,10 +3,11 @@ import streamlit as st
 
 classifier = pipeline("sentiment-analysis", "blanchefort/rubert-base-cased-sentiment")
 st.title('Анализ тональности текста сообщений из группы телеграмм')
+path_to_strings = 'chats.csv'
 
 
 def strings_analize():
-    with open('chats.csv', 'r+', encoding="UTF-8") as df:
+    with open(path_to_strings, 'r+', encoding="UTF-8") as df:
         for line in df:
             category = classifier([line])
             print(line, category)
